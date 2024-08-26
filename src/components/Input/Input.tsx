718ac341103716e358ba05145416587c83e2de1f
Van Hung <hungdongky2003@gmail.com>
2024-08-26 23:49:21 +0700

    B180

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,14 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { InputHTMLAttributes } from 'react'
 import { UseFormRegister, RegisterOptions } from 'react-hook-form'
-interface Props {
-  type: React.HTMLInputTypeAttribute
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
   errorMessage?: string
-  placeholder?: string
-  className?: string
-  name: string
-  register: UseFormRegister<any>
+  register?: UseFormRegister<any>
+  classNameInput?: string
+  classNameError?: string
   rules?: RegisterOptions
-  autoComplete?: string
 }
 
 export default function Input({
@@ -16,21 +14,25 @@ export default function Input({
   errorMessage,
   placeholder,
   className,
+  autoComplete,
   name,
   register,
   rules,
-  autoComplete
+  classNameInput = 'p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm',
+
+  classNameError = 'mt-1 text-red-600 min-h-[1.25rem] text-sm'
 }: Props) {
+  const registerResult = register && name ? register(name, rules) : {}
   return (
     <div className={className}>
       <input
         type={type}
         placeholder={placeholder}
-        className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
-        {...register(name, rules)}
+        className={classNameInput}
+        {...registerResult}
         autoComplete={autoComplete}
       />
-      <div className='mt-1 text-red-600 min-h-[1.25rem] text-sm'>{errorMessage}</div>
+      <div className={classNameError}>{errorMessage}</div>
     </div>
   )
 }
--- src/pages/ProductList/ProductList.tsx
+++ src/pages/ProductList/ProductList.tsx
@@ -1,6 +1,40 @@
+import useQueryParams from 'src/hooks/useQueryParams'
+import AsideFilter from './AsideFilter'
+import Product from './Product/Product'
+import SortProductList from './SortProductList'
+import { useQuery } from '@tanstack/react-query'
+import productApi from 'src/apis/product.api'
 
 export default function ProductList() {
+  const queryParams = useQueryParams()
+  const { data } = useQuery({
+    queryKey: ['products', queryParams],
+    queryFn: () => {
+      return productApi.getProducts(queryParams)
+    }
+  })
+  console.log(data)
+
   return (
-    <div>ProductList</div>
+    <div className='bg-gray-200 py-6'>
+      <div className='container'>
+        <div className='grid grid-cols-12 gap-6'>
+          <div className='col-span-2'>
+            <AsideFilter />
+          </div>
+          <div className='col-span-10'>
+            <SortProductList />
+            <div className='mt-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3'>
+              {data &&
+                data.data.data.products.map((product) => (
+                  <div className='col-span-1' key={product._id}>
+                    <Product product={product} />
+                  </div>
+                ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
