9f7eb19b9789d711d46c6e68c5fd933769c26675
Van Hung <hungdongky2003@gmail.com>
2024-08-28 17:45:09 +0700

    B186

---
+++ src/components/InputNumber/InputNumber.tsx
@@ -0,0 +1,36 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { forwardRef, InputHTMLAttributes } from 'react'
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
+  errorMessage?: string
+  classNameInput?: string
+  classNameError?: string
+}
+
+const InputNumber = forwardRef<HTMLInputElement, Props>(function InputNumberInner(
+  {
+    errorMessage,
+    className,
+    classNameInput = 'p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm',
+    classNameError = 'mt-1 text-red-600 min-h-[1.25rem] text-sm',
+    onChange,
+    ...rest
+  },
+  ref
+) {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target
+    if (/^\d+$/.test(value) || value === '') {
+      // Thực thi onChange callback từ bên ngoài truyền vào props
+      onChange(event)
+      // Cập nhật localValue state
+    }
+  }
+  return (
+    <div className={className}>
+      <input className={classNameInput} onChange={handleChange} {...rest} ref={ref} />
+      <div className={classNameError}>{errorMessage}</div>
+    </div>
+  )
+})
+
+export default InputNumber
